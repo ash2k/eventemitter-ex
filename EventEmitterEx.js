70c424f3f4d94469e6600d0ab849619024c77bb7
Mikhail Mazurskiy <ash2kk@gmail.com>
2015-03-14 15:31:46 +1100

    Bugfixes and more tests for pipeExcept

--- EventEmitterEx.js
+++ EventEmitterEx.js
@@ -1,30 +1,74 @@
 (function () {
     'use strict';
 
-    var EE = require('events').EventEmitter;
+    var EE = require('events').EventEmitter,
+        util = require('util');
 
     module.exports = EventEmitterEx;
 
     function EventEmitterEx () {
         EE.call(this);
+        this._onAllListeners = [];
+        this._originalEmit = this.emit;
+        this.emit = emit;
     }
-    require('util').inherits(EventEmitterEx, EE);
+    util.inherits(EventEmitterEx, EE);
 
-    EventEmitterEx.prototype.pipeExcept = function pipeExcept (ee, except) {
-        var self = this;
-        var emit = ee.emit;
-        except = except || [];
-        ee.emit = function (event /* arguments */) {
-            var res = false;
-            if (event === 'error') {
-                if (EE.listenerCount(ee, event)) res = emit.apply(ee, arguments);
-                if (! res || EE.listenerCount(self, event)) res = self.emit.apply(self, arguments);
-            } else {
-                res = emit.apply(ee, arguments);
-                res = except.indexOf(event) === -1 ? self.emit.apply(self, arguments) || res : res;
+    EventEmitterEx.prototype.onAllExcept = function onAllExcept (f /* arguments */) {
+        if (typeof f !== 'function')
+            throw new Error('Listener must be a function. Got ' + typeof f);
+        var except = Array.prototype.slice.call(arguments, 1);
+        this._onAllListeners.push([f, except]);
+    };
+
+    function emit (type /* arguments */) {
+        var args = arguments,
+            res = false,
+            filtered = this.listenersOnAll(type);
+
+        if (type !== 'error' || filtered.length === 0 || EE.listenerCount(this, type)) {
+            res = this._originalEmit.apply(this, args);
+        }
+
+        filtered.forEach(function (listener) {
+            listener.apply(null, args);
+        });
+
+        return res || filtered.length > 0;
+    }
+
+    EventEmitterEx.prototype.pipeExcept = function pipeExcept (ee) {
+        var self = this,
+            except = Array.prototype.slice.call(arguments, 1);
+
+        if (typeof ee.onAllExcept === 'function') {
+            // This is an EventEmitterEx
+            except.unshift(function (/* arguments */) {
+                self.emit.apply(self, arguments);
+            });
+            ee.onAllExcept.apply(ee, except);
+        } else if (typeof ee.emit === 'function') {
+            // This is a usual EventEmitter
+            var emit = ee.emit;
+            if (except.indexOf('error') === -1) {
+                ee.on('error', function (/* arguments */) {
+                    var args = Array.prototype.slice.call(arguments);
+                    args.unshift('error');
+                    self.emit.apply(self, args);
+                });
             }
-            return  res;
-        };
+
+            ee.emit = function pipedEmit (type /* arguments */) {
+                var args = arguments,
+                    res = emit.apply(ee, args);
+                if (type !== 'error' && except.indexOf(type) === -1) {
+                    res = self.emit.apply(self, args) || res;
+                }
+                return  res;
+            };
+        } else {
+            throw new TypeError('Expecting EventEmitter or EventEmitterEx. Given: ' + typeof ee);
+        }
     };
 
     EventEmitterEx.prototype.flatMap = function flatMap (f) {
@@ -36,6 +80,24 @@
         });
 
         return res;
+    };
+
+    EventEmitterEx.prototype.listenersOnAll = function listenersOnAll (type) {
+        return this._onAllListeners
+            .filter(function (listener) {
+                return listener[1].indexOf(type) === -1;
+            })
+            .map(function (listener) {
+                return listener[0];
+            });
+    };
+
+    EventEmitterEx.prototype.listenerCountOnAll = function listenerCountOnAll (type) {
+        return this.listenersOnAll(type).length;
+    };
+
+    EventEmitterEx.listenerCount = function listenerCount (eex, type) {
+        return (typeof eex.listenerCountOnAll === 'function' ? eex.listenerCountOnAll(type) : 0) + EE.listenerCount(eex, type);
     };
 
 })();
--- test/EventEmitterEx.test.js
+++ test/EventEmitterEx.test.js
@@ -1,187 +1,294 @@
 (function () {
     'use strict';
     var EventEmitter = require('events').EventEmitter
-        , EEP = require('../EventEmitterEx')
-        , source
+        , EEX = require('../EventEmitterEx')
         , emitter
         ;
 
     beforeEach(function () {
-        source = new EventEmitter();
-        emitter = new EEP();
+        emitter = new EEX();
     });
 
     describe('EventEmitterEx', function () {
         describe('#pipeExcept()', function () {
 
-            it('should call corresponding callbacks on original emitter', function () {
-                var spyA = sinon.spy()
-                    , spyError1 = sinon.spy()
-                    , spyError2 = sinon.spy()
-                    , spyEnd = sinon.spy()
-                    , err = new Error('123');
+            var i = 0;
+            [EventEmitter, EEX].forEach(function (sourceType) {
+                i++;
+                var source;
 
-                emitter.pipeExcept(source, 'end');
+                beforeEach(function () {
+                    source = new sourceType()
+                });
 
-                emitter.on('error', spyError1);
+                it('should call corresponding callbacks on original emitter #' + i, function () {
+                    var spyA = sinon.spy()
+                        , spyError1 = sinon.spy()
+                        , spyError2 = sinon.spy()
+                        , spyEnd = sinon.spy()
+                        , err = new Error('123');
 
-                source.on('a', spyA);
-                source.on('end', spyEnd);
-                source.on('error', spyError2);
+                    source.on('a', spyA);
+                    source.on('end', spyEnd);
+                    source.on('error', spyError1);
 
-                source.emit('a', 'a1').should.be.true;
-                source.emit('end', 1, 2, 3).should.be.true;
-                source.emit('error', err).should.be.true;
+                    emitter.pipeExcept(source, 'end');
 
-                spyA.calledWith('a1').should.be.true;
-                spyEnd.calledWith(1, 2, 3).should.be.true;
-                spyError1.calledWith(err).should.be.true;
-                spyError2.calledWith(err).should.be.true;
+                    emitter.on('error', spyError2);
+
+                    source.emit('a', 'a1').should.be.true;
+                    source.emit('end', 1, 2, 3).should.be.true;
+                    source.emit('error', err).should.be.true;
+
+                    spyA.calledWith('a1').should.be.true;
+                    spyEnd.calledWith(1, 2, 3).should.be.true;
+                    spyError1.calledWith(err).should.be.true;
+                    spyError2.calledWith(err).should.be.true;
+
+                    spyEnd.calledAfter(spyA).should.be.true;
+                    spyError1.calledAfter(spyEnd).should.be.true;
+                    spyError2.calledAfter(spyError1).should.be.true;
+                });
+
+                it('should pipe all events except specified #' + i, function () {
+                    var spyA = sinon.spy()
+                        , spyError1 = sinon.spy()
+                        , spyError2 = sinon.spy()
+                        , spyEnd = sinon.spy()
+                        , err = new Error('123');
+
+                    source.on('error', spyError1);
+
+                    emitter.pipeExcept(source, 'end', 'x');
+
+                    emitter.on('a', spyA);
+                    emitter.on('end', spyEnd);
+                    emitter.on('error', spyError2);
+
+                    source.emit('a', 'a1').should.be.true;
+                    source.emit('end', 1, 2, 3).should.be.false;
+                    source.emit('error', err).should.be.true;
+
+                    spyA.calledWith('a1').should.be.true;
+                    spyEnd.calledWith(1, 2, 3).should.be.false;
+                    spyError1.calledWith(err).should.be.true;
+                    spyError2.calledWith(err).should.be.true;
+
+                    spyError1.calledAfter(spyA).should.be.true;
+                    spyError2.calledAfter(spyError1).should.be.true;
+                });
+
+                it('should not throw on unhandled error on original emitter #' + i, function () {
+                    var spyError = sinon.spy()
+                        , err = new Error('123');
+
+                    emitter.pipeExcept(source);
+
+                    emitter.on('error', spyError);
+
+                    source.emit('error', err).should.be.true;
+
+                    spyError.calledWith(err).should.be.true;
+                });
+
+                it('should throw on unhandled error on piped emitter #' + i, function () {
+                    var spyError = sinon.spy()
+                        , err = new Error('123');
+
+                    source.on('error', spyError);
+
+                    emitter.pipeExcept(source);
+
+                    expect(function () {
+                        source.emit('error', err);
+                    }).to.throw(err);
+
+                    spyError.calledWith(err).should.be.true;
+                });
+
+                it('should not emit error if it is excepted #' + i, function () {
+                    var spyError1 = sinon.spy()
+                        , spyError2 = sinon.spy()
+                        , err = new Error('123');
+
+                    emitter.pipeExcept(source, 'error');
+
+                    source.on('error', spyError1);
+                    emitter.on('error', spyError2);
+
+                    source.emit('error', err).should.be.true;
+
+                    spyError1.calledWith(err).should.be.true;
+                    spyError2.callCount.should.be.equal(0);
+                });
+
+                it('should not throw error if it is excepted #' + i, function () {
+                    var spyError = sinon.spy()
+                        , err = new Error('123');
+
+                    emitter.pipeExcept(source, 'error');
+
+                    source.on('error', spyError);
+
+                    source.emit('error', err).should.be.true;
+
+                    spyError.calledWith(err).should.be.true;
+                });
+
+                it('should throw on unhandled errors #' + i, function () {
+                    var err = new Error('123');
+
+                    emitter.pipeExcept(source);
+
+                    expect(function () {
+                        source.emit('error', err);
+                    }).to.throw(err);
+                });
+
+                var j = 0;
+                [EventEmitter, EEX].forEach(function (sourceType2) {
+                    j++;
+                    it('should pipe events from multiple sources #' + i + ':' + j, function () {
+                        var spyError = sinon.spy()
+                            , spyEnd = sinon.spy()
+                            , err = new Error('123')
+                            , source2 = new sourceType2()
+                            ;
+
+                        emitter.pipeExcept(source);
+                        emitter.pipeExcept(source2);
+                        emitter.on('error', spyError);
+                        emitter.on('end', spyEnd);
+
+                        source.emit('error', err).should.be.true;
+                        source2.emit('end', 1, 2, 3).should.be.true;
+
+                        spyError.calledWith(err).should.be.true;
+                        spyEnd.calledWith(1, 2, 3).should.be.true;
+
+                        spyEnd.calledAfter(spyError).should.be.true;
+                    });
+                });
+
+                it('should support multiple pipes from single source #' + i, function () {
+                    var spyError = sinon.spy()
+                        , spyEnd = sinon.spy()
+                        , spyError2 = sinon.spy()
+                        , spyEnd2 = sinon.spy()
+                        , err = new Error('123')
+                        , emitter2 = new EEX()
+                        ;
+
+                    emitter.pipeExcept(source);
+                    emitter2.pipeExcept(source);
+                    emitter.on('error', spyError);
+                    emitter.on('end', spyEnd);
+                    emitter2.on('error', spyError2);
+                    emitter2.on('end', spyEnd2);
+
+                    source.emit('error', err).should.be.true;
+                    source.emit('end', 1, 2, 3).should.be.true;
+
+                    spyError.calledWith(err).should.be.true;
+                    spyEnd.calledWith(1, 2, 3).should.be.true;
+                    spyError2.calledWith(err).should.be.true;
+                    spyEnd2.calledWith(1, 2, 3).should.be.true;
+
+                    spyError2.calledAfter(spyError).should.be.true;
+                    spyEnd2.calledAfter(spyEnd).should.be.true;
+                });
             });
 
-            it('should pipe all events except specified', function () {
-                var spyA = sinon.spy()
-                    , spyError1 = sinon.spy()
-                    , spyError2 = sinon.spy()
-                    , spyEnd = sinon.spy()
-                    , err = new Error('123');
-
-                emitter.pipeExcept(source, 'end');
-
-                source.on('error', spyError1);
-
-                emitter.on('a', spyA);
-                emitter.on('end', spyEnd);
-                emitter.on('error', spyError2);
-
-                source.emit('a', 'a1').should.be.true;
-                source.emit('end', 1, 2, 3).should.be.false;
-                source.emit('error', err).should.be.true;
-
-                spyA.calledWith('a1').should.be.true;
-                spyEnd.calledWith(1, 2, 3).should.be.false;
-                spyError1.calledWith(err).should.be.true;
-                spyError2.calledWith(err).should.be.true;
-            });
-
-            it('should not throw on unhandled error on original emitter', function () {
-                var spyError = sinon.spy()
-                    , err = new Error('123');
-
-                emitter.pipeExcept(source);
-
-                emitter.on('error', spyError);
-
-                source.emit('error', err).should.be.true;
-
-                spyError.calledWith(err).should.be.true;
-            });
-
-            it('should not throw on unhandled error on piped emitter', function () {
-                var spyError = sinon.spy()
-                    , err = new Error('123');
-
-                emitter.pipeExcept(source);
-
-                source.on('error', spyError);
-
-                source.emit('error', err).should.be.true;
-
-                spyError.calledWith(err).should.be.true;
-            });
-
-            it('should throw on unhandled errors', function () {
-                var err = new Error('123');
-
-                emitter.pipeExcept(source);
-
-                expect(function () {
-                    source.emit('error', err);
-                }).to.throw(err);
-            });
         });
 
         describe('#flatMap()', function () {
 
-            it('should pipe all events and call map function', function (done) {
-                var spyData = sinon.spy()
-                    , spyInfo = sinon.spy()
-                    , spyMsg = sinon.spy()
-                    , A = 1, B = 40, C = 2, DATA = 24, MSG = 'FP (allthethings)';
+            var i = 0;
+            [[EventEmitter, EventEmitter], [EventEmitter, EEX], [EEX, EventEmitter], [EEX, EEX]]
+                .forEach(function (sourceTypes) {
+                    i++;
+                    it('should pipe all events and call map function #' + i, function (done) {
+                        var spyData = sinon.spy()
+                            , spyInfo = sinon.spy()
+                            , spyMsg = sinon.spy()
+                            , A = 1, B = 40, C = 2, DATA = 24, MSG = 'FP (allthethings)';
 
-                emitter
-                    .flatMap(function (a, b, c) {
-                        var emitter2 = new EEP();
+                        emitter
+                            .flatMap(function (a, b, c) {
+                                var emitter2 = new sourceTypes[0]();
 
-                        setImmediate(function () {
-                            emitter2.emit('info', a);
-                            emitter2.emit('end', b, c);
-                        });
+                                setImmediate(function () {
+                                    emitter2.emit('info', a);
+                                    emitter2.emit('end', b, c);
+                                });
 
-                        return emitter2;
-                    })
-                    .flatMap(function (b, c) {
-                        var emitter3 = new EEP();
+                                return emitter2;
+                            })
+                            .flatMap(function (b, c) {
+                                var emitter3 = new sourceTypes[1]();
 
-                        setImmediate(function () {
-                            emitter3.emit('data', DATA);
-                            emitter3.emit('end', b + c);
-                        });
+                                setImmediate(function () {
+                                    emitter3.emit('data', DATA);
+                                    emitter3.emit('end', b + c);
+                                });
 
-                        return emitter3;
+                                return emitter3;
 
-                    })
-                    .on('error', done)
-                    .on('msg', spyMsg)
-                    .on('info', spyInfo)
-                    .on('data', spyData)
-                    .on('end', function (d) {
-                        spyMsg.calledWith(MSG).should.be.true;
-                        spyInfo.calledWith(A).should.be.true;
-                        spyData.calledWith(DATA).should.be.true;
-                        d.should.equal(B + C);
-                        done();
+                            })
+                            .on('error', done)
+                            .on('msg', spyMsg)
+                            .on('info', spyInfo)
+                            .on('data', spyData)
+                            .on('end', function (d) {
+                                spyMsg.calledWith(MSG).should.be.true;
+                                spyInfo.calledWith(A).should.be.true;
+                                spyData.calledWith(DATA).should.be.true;
+                                d.should.equal(B + C);
+                                done();
+                            });
+
+                        emitter.emit('msg', MSG);
+                        emitter.emit('end', A, B, C);
                     });
+                });
 
-                emitter.emit('msg', MSG);
-                emitter.emit('end', A, B, C);
+            i = 0;
+            [EventEmitter, EEX].forEach(function (sourceType) {
+                i++;
+                it('should propagate error event and do not call next map function #' + i, function (done) {
+                    var spyMapper = sinon.spy()
+                        , spyInfo = sinon.spy()
+                        , spyMsg = sinon.spy()
+                        , A = 1, B = 40, C = 2, MSG = 'FP (allthethings)'
+                        , ERR = new Error('Something fishy just happened!');
+
+                    emitter
+                        .flatMap(function (a, b, c) {
+                            var emitter2 = new sourceType();
+
+                            setImmediate(function () {
+                                emitter2.emit('info', a);
+                                emitter2.emit('error', ERR);
+                            });
+
+                            return emitter2;
+                        })
+                        .flatMap(spyMapper)
+                        .on('error', function (err) {
+                            spyMsg.calledWith(MSG).should.be.true;
+                            spyInfo.calledWith(A).should.be.true;
+                            spyMapper.callCount.should.be.equal(0);
+                            err.should.equal(ERR);
+                            done();
+                        })
+                        .on('msg', spyMsg)
+                        .on('info', spyInfo)
+                        .on('end', done.bind(null, new Error('Should not happen')));
+
+                    emitter.emit('msg', MSG);
+                    emitter.emit('end', A, B, C);
+                });
             });
-
-
-            it('should propagate error event and do not call next map function', function (done) {
-                var spyMapper = sinon.spy()
-                    , spyInfo = sinon.spy()
-                    , spyMsg = sinon.spy()
-                    , A = 1, B = 40, C = 2, MSG = 'FP (allthethings)'
-                    , ERR = new Error('Something fishy just happened!');
-
-                emitter
-                    .flatMap(function (a, b, c) {
-                        var emitter2 = new EEP();
-
-                        setImmediate(function () {
-                            emitter2.emit('info', a);
-                            emitter2.emit('error', ERR);
-                        });
-
-                        return emitter2;
-                    })
-                    .flatMap(spyMapper)
-                    .on('error', function (err) {
-                        spyMsg.calledWith(MSG).should.be.true;
-                        spyInfo.calledWith(A).should.be.true;
-                        spyMapper.callCount.should.be.equal(0);
-                        err.should.equal(ERR);
-                        done();
-                    })
-                    .on('msg', spyMsg)
-                    .on('info', spyInfo)
-                    .on('end', done.bind(null, new Error('Should not happen')));
-
-                emitter.emit('msg', MSG);
-                emitter.emit('end', A, B, C);
-            });
-
         });
 
     });
